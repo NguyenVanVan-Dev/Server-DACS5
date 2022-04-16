d72b6069b3c4308167b7bda133e837ef934c5257
Nguyễn Văn Vấn <nvvan.0901.developer@gmail.com>
2022-04-16 19:12:37 +0700

    fix import file

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const db = require('./src/app/config');
+const db = require('./src/app/config/index');
 const route = require('./src/Routes');
 const cors = require('cors')
 const morgan = require('morgan');
