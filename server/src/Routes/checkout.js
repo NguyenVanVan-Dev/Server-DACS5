900b5577de2f528eb22bea3ed8376ac8ae14ae7c
Nguyễn Văn Vấn <nvvan.0901.developer@gmail.com>
2022-04-21 11:48:56 +0700

    remove middleware auth with checkout

--- server/src/Routes/checkout.js
+++ server/src/Routes/checkout.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const CheckoutController = require('../App/Controllers/CheckoutController');
-const authMiddleware = require('../App/Middleware/Authtization');
 router.post('/store',CheckoutController.store);
 router.get('/populate',CheckoutController.populateData);
 
